7bbdca810845a81bdb08722621d3d9d7f2c021c2
김지은 <sallykim2018@naver.com>
2022-10-06 13:16:55 +0900

    my final commit

--- app/src/main/java/org/sopt/sample/MainActivity.kt
+++ app/src/main/java/org/sopt/sample/MainActivity.kt
@@ -2,9 +2,17 @@ package org.sopt.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.databinding.ActivityIntroduceBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroduceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityIntroduceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewId.text= intent.getStringExtra("sample_id")
+        binding.textViewMbti.text= intent.getStringExtra("sample_mbti")
     }
 }
