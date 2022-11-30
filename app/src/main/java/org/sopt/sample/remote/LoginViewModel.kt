9e47fc7915071fffe36feeeceb47cf95270e2c55
김지은 <sallykim2018@naver.com>
2022-11-30 19:46:31 +0900

    [fix] LoginViewModel 확장함수로 변환

--- app/src/main/java/org/sopt/sample/remote/LoginViewModel.kt
+++ app/src/main/java/org/sopt/sample/remote/LoginViewModel.kt
@@ -4,13 +4,17 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.sopt.sample.data.dto.request.RequestLoginDTO
+import org.sopt.sample.data.dto.response.ResponseLoginDTO
+import org.sopt.sample.util.enqueueUtil
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import timber.log.Timber
 
 class LoginViewModel : ViewModel() {
-    private val _loginResult=MutableLiveData<ResponseLoginDTO>()
-    val loginResult:LiveData<ResponseLoginDTO>
+    private val _loginResult=MutableLiveData<ResponseLoginDTO?>()
+    val loginResult:LiveData<ResponseLoginDTO?>
     get()=_loginResult
     private val _errorMessage = MutableLiveData<String>()
     val errorMessage: LiveData<String>
@@ -19,22 +23,11 @@ class LoginViewModel : ViewModel() {
 
     fun login(email: String, password: String){
         loginService.login(RequestLoginDTO(email,password))
-            .enqueue(object : Callback<ResponseLoginDTO>{
-                override fun onResponse(
-                    call: Call<ResponseLoginDTO>,
-                    response: Response<ResponseLoginDTO>
-                ) {
-                    if(response.isSuccessful){
-                        _loginResult.value=response.body()
-                    }else{
-                        _errorMessage.value="기본값"
-                    }
-                }
-
-                override fun onFailure(call: Call<ResponseLoginDTO>, t: Throwable) {
-                    Log.e("LOGIN_FAIL","cause : "+t.cause)
-                    Log.e("LOGIN_FAIL","cause : "+t.message)
-                }
+            .enqueueUtil({ response ->
+                _loginResult.value = response.result
+                Timber.i("로그인 성공: ${response.result}")
+            }, { code ->
+                Timber.d("로그인 실패: $code")
             })
     }
 }
